bf3662cfb56f968c3e82e39c488b95ceed7e31c7
Vladimir Svetlakov <vova.svetl@mail.ru>
2023-05-26 14:53:48 +0300

    Добавлен базовый интерфейс

--- src/main/java/controller/TimerController.java
+++ src/main/java/controller/TimerController.java
@@ -3,6 +3,10 @@ package controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
 public class TimerController {
@@ -11,10 +15,14 @@ public class TimerController {
     @FXML
     private Button startButton;
 
+    @FXML
+    Pane pane;
+
     private CountdownTimer countdownTimer;
 
     public void initialize() {
         countdownTimer = new CountdownTimer(this::updateTimerLabel);
+        pane.setBackground(new Background(new BackgroundFill(Color.GRAY,null,null)));
     }
 
     @FXML
@@ -25,6 +33,6 @@ public class TimerController {
 
     private void updateTimerLabel(String time) {
         timerLabel.setText(time);
-        timerLabel.setFont(Font.font("Arial", 24)); // Установка размера шрифта 24
+        //timerLabel.setFont(Font.font("Arial", 24)); // Установка размера шрифта 24
     }
 }
