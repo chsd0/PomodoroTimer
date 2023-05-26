70ac330f35380166b0cbbf354cb7a16f3a6cd890
Mikhail <sarancha_mikhail@mail.ru>
2023-05-26 13:50:40 +0300

    Реализована система помидора - циклическая работа таймера с выводом оставшегося времени работы на экран

--- src/main/java/controller/CountdownTimer.java
+++ src/main/java/controller/CountdownTimer.java
@@ -1,33 +1,74 @@
 package controller;
-import java.util.Timer;
-import java.util.TimerTask;
-import javafx.application.Platform;
-import javafx.scene.control.Label;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.util.Duration;
 
 public class CountdownTimer {
-    private Timer timer;
-    private int seconds;
-    private Label label;
+    private static final int WORK_TIME = 25; // Время работы в минутах
+    private static final int SHORT_BREAK_TIME = 5; // Время короткого перерыва в минутах
+    private static final int LONG_BREAK_TIME = 20; // Время длительного перерыва в минутах
+    private static final int CYCLES_BEFORE_LONG_BREAK = 4; // Количество циклов работы до длительного перерыва
 
-    public CountdownTimer(int seconds, Label label) {
-        this.seconds = seconds;
-        this.label = label;
+    private int cycleCount = 0;
+    private int currentTime;
+    private boolean isWorkingTime = true;
+    private TimerListener listener;
+    private Timeline timeline;
+
+    public CountdownTimer(TimerListener listener) {
+        this.listener = listener;
     }
 
     public void start() {
-        label.setText("ОВфроОРВРООЫВ");
-        timer = new Timer();
-        timer.scheduleAtFixedRate(new TimerTask() {
-            public void run() {
-                if (seconds <= 0) {
-                    timer.cancel();
+        if (isWorkingTime) {
+            currentTime = WORK_TIME * 60;
+            listener.onTimerUpdate(formatTime(currentTime));
+        } else {
+            currentTime = SHORT_BREAK_TIME * 60;
+            listener.onTimerUpdate(formatTime(currentTime));
+        }
+
+        timeline = new Timeline(new KeyFrame(Duration.seconds(1), e -> {
+            currentTime--;
+            if (currentTime <= 0) {
+                cycleCount++;
+                if (isWorkingTime) {
+                    if (cycleCount >= CYCLES_BEFORE_LONG_BREAK) {
+                        cycleCount = 0;
+                        currentTime = LONG_BREAK_TIME * 60;
+                        listener.onTimerUpdate(formatTime(currentTime));
+                    } else {
+                        currentTime = SHORT_BREAK_TIME * 60;
+                        listener.onTimerUpdate(formatTime(currentTime));
+                    }
                 } else {
-                    Platform.runLater(() -> {
-                        label.setText(String.valueOf(seconds));
-                    });
-                    seconds--;
+                    currentTime = WORK_TIME * 60;
+                    listener.onTimerUpdate(formatTime(currentTime));
                 }
+                isWorkingTime = !isWorkingTime;
+            } else {
+                listener.onTimerUpdate(formatTime(currentTime));
             }
-        }, 0, 1000);
+        }));
+
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+    }
+
+    private String formatTime(int timeInSeconds) {
+        int minutes = timeInSeconds / 60;
+        int seconds = timeInSeconds % 60;
+        return String.format("%02d:%02d", minutes, seconds);
+    }
+
+    public void stop() {
+        if (timeline != null) {
+            timeline.stop();
+        }
+    }
+
+    public interface TimerListener {
+        void onTimerUpdate(String time);
     }
 }
