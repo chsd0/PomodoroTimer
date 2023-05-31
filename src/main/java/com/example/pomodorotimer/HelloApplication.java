2f137c5448a2ed3a6737c4cd3210f0d147f5a9fd
PudgeUltimate <royaleplayer3@gmail.com>
2023-05-31 19:34:28 +0300

    Icon added

--- src/main/java/com/example/pomodorotimer/HelloApplication.java
+++ src/main/java/com/example/pomodorotimer/HelloApplication.java
@@ -2,7 +2,6 @@ package com.example.pomodorotimer;
 
 import controller.TimerController;
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -10,7 +9,7 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Timer;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
@@ -19,12 +18,12 @@ public class HelloApplication extends Application {
         Parent root = fxmlLoader.load();
         TimerController timerController = fxmlLoader.getController();
         Scene scene = new Scene(root);
+
         stage.setTitle("Pomodoro Timer");
-        stage.getIcons().add(new Image("/path/to/stackoverflow.jpg"));
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/помидор.png")));
+
         stage.setScene(scene);
         stage.show();
-
-
     }
 
     public static void main(String[] args) {
